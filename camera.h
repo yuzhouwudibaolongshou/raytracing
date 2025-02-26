b3094bf5cf50794cea3d6f39439bc06f8af8088f
Sean <571175497@qq.com>
2025-02-26 17:11:25 +0800

    抗锯齿

--- camera.h
+++ camera.h
@@ -12,6 +12,7 @@ class camera {
     // Image
     double aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
+    int    samples_per_pixel = 10;   // Count of random samples for each pixel
 
     void render(const hittable& world) {
         initialize();
@@ -22,12 +23,12 @@ class camera {
     for (int j = 0; j < image_height; j++) {
         std::cout << "Scanlines remaining: " << (image_height - j) << std::endl;
         for (int i = 0; i < image_width; i++) {
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - center;
-            ray r(center, ray_direction);
-
-            color pixel_color = ray_color(r, world);
-            write_color(ppm, pixel_color);
+            color pixel_color(0,0,0);
+                for (int sample = 0; sample < samples_per_pixel; sample++) {
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, world);
+                }
+                write_color(ppm, pixel_samples_scale * pixel_color);
         }
     }
 
@@ -36,6 +37,7 @@ class camera {
 
   private:
     int    image_height;   // Rendered image height
+    double pixel_samples_scale;  // Color scale factor for a sum of pixel samples
     point3 center;         // Camera center
     point3 pixel00_loc;    // Location of pixel 0, 0
     vec3   pixel_delta_u;  // Offset to pixel to the right
@@ -44,6 +46,8 @@ class camera {
     void initialize() {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
+
+        pixel_samples_scale = 1.0 / samples_per_pixel;
 
         center = point3(0, 0, 0);
 
@@ -66,6 +70,26 @@ class camera {
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
+    ray get_ray(int i, int j) const {
+        // Construct a camera ray originating from the origin and directed at randomly sampled
+        // point around the pixel location i, j.
+
+        auto offset = sample_square();
+        auto pixel_sample = pixel00_loc
+                          + ((i + offset.x()) * pixel_delta_u)
+                          + ((j + offset.y()) * pixel_delta_v);
+
+        auto ray_origin = center;
+        auto ray_direction = pixel_sample - ray_origin;
+
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 sample_square() const {
+        // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
+        return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+    }
+    
     color ray_color(const ray& r, const hittable& world/*这是一个抽象的接口，代表场景中的所有物体集合（hittable_list类）*/) {
     hit_record rec;//交点坐标、法向量normal、t值
     if (world.hit(r, interval(0, infinity), rec)/*指定t范围内判定是否相交的函数*/) {
