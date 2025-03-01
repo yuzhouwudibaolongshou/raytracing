3fc57174ec815034cc2af2b87ecbe3a87b577339
Sean <571175497@qq.com>
2025-03-01 14:00:18 +0800

    完成了一个空心玻璃球的定义

--- main.cpp
+++ main.cpp
@@ -17,8 +17,10 @@ int main() {
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     //auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
     //auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
-    //auto material_left   = make_shared<dielectric>(1.50);//1.5表示refraction_index折射率，即从折射率为1的材质中射入为1.5的材质中
-    auto material_left   = make_shared<dielectric>(1.00 / 1.33);//此时为两个材料的折射率。这会被用来判断是否折射
+    //auto material_left   = make_shared<dielectric>(1.50);//1.5表示refraction_index折射率之比，即从折射率为1的材质中射入为1.5的材质中
+    //auto material_left   = make_shared<dielectric>(1.00 / 1.33);//此时为两个材料的折射率。这会被用来判断是否折射
+    auto material_left   = make_shared<dielectric>(1.50);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);//在玻璃球内部实现一个空气球，此时界面处的折射率之比为“1/1.5”
     auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);//加上了毛金属系数（可以理解为镜面反射与散射之比，算是这两者的“菲涅尔项”）
 
     //world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
@@ -27,6 +29,7 @@ int main() {
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     camera cam;
